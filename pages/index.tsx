108f8a2de2c49383733b28ff8a7d9b9c253677fa
abdulazizbay7 <abdulazizxalilov30@gmail.com>
2023-02-17 17:45:52 +0900

    updated index.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Navbar from '../src/components/navbar/Navbar'
 import TitleSalesForceLab from '../src/components/titleSalesForceLab/titleSalesForceLab'
 import Head from 'next/head'
 
+
 export default function Home() {
   return (
     <>
@@ -9,7 +10,6 @@ export default function Home() {
         <title>Apexlab | Salesforce</title>
         <meta name="keywords" content="apexlab,programming jobs, freelance" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet='utf-8' />
       </Head>
       <main >
         <Navbar/>
